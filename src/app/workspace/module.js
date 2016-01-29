b39cbdac9cb01e63fabd3ed6d54d037a2c3346a0
Sébastien Mischler (aka skarab) <skarab@bluewin.ch>
2016-01-29 11:56:11 +0100

    add panel manager

--- src/app/workspace/module.js
+++ src/app/workspace/module.js
@@ -15,5 +15,45 @@ var AppWorkspace = GuiModule.extend(
     {
         // Bind model with main view
         this.model && ko.applyBindings(this.model);
+    },
+
+    /**
+    * Add a new panel
+    *
+    * @event  addPanel
+    * @param  {StrawModule} module
+    * @param  {Mixed}       result The value returned by module.setup().
+    */
+    addPanel: function(module)
+    {
+        var column = this.model[module.model.target()];
+
+        // Push to obserable array
+        column.push(module.model);
+
+        // Sort panels in user order
+        column.sort(function(left, right)
+        {
+            if (left.index() == right.index()) return  0;
+            if (left.index() >  right.index()) return  1;
+            if (left.index() <  right.index()) return -1;
+        });
+
+        this.message('info', 'panelAdded', { panel: module.name });
+    },
+
+    /**
+    * Called after a module is setup.
+    *
+    * @event  onSetup
+    * @param  {StrawModule} module
+    * @param  {Mixed}       result The value returned by module.setup().
+    */
+    onSetup: function(module, result)
+    {
+        // New panel ready
+        if (module instanceof GuiPanel) {
+            this.addPanel(module);
+        }
     }
 });
--- src/app/workspace/views/body.tpl
+++ src/app/workspace/views/body.tpl
@@ -3,8 +3,8 @@
     <!-- Two columns grid -->
     <div class="row">
         <!-- Column 1 -->
-        <div data-bind="template: { name: 'app-panel-tpl', foreach: columnOne }" id="column-one" class="col col-md-4"></div>
+        <div data-bind="template: { name: 'guipanel-main-tpl', foreach: columnOne }" id="column-one" class="col col-md-4"></div>
         <!-- Column 2 -->
-        <div data-bind="template: { name: 'app-panel-tpl', foreach: columnTwo }" id="column-two" class="col col-md-8"></div>
+        <div data-bind="template: { name: 'guipanel-main-tpl', foreach: columnTwo }" id="column-two" class="col col-md-8"></div>
     </div>
 </div>
