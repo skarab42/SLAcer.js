243fd3e40395dc615b51380fafb11099ebc35416
Sébastien Mischler (aka skarab) <skarab@bluewin.ch>
2016-03-18 08:44:53 +0100

    make an clean UI

--- js/main.js
+++ js/main.js
@@ -1,15 +1,36 @@
 // -----------------------------------------------------------------------------
 // Variables
 // -----------------------------------------------------------------------------
-var meshMaterial = new THREE.MeshPhongMaterial({
-    color: 0xff0000
+var settings = new SLAcer.Settings({
+    buildVolume: {
+        size     : { x: 100,  y: 100,  z: 100 }, // mm
+        unit     : 'mm',                         // mm or in
+        color    : 0xcccccc,
+        opacity  : 0.1,
+        collapsed: false
+    },
+    resin: {
+        density  : 1.1, // g/cm3
+        price    : 50,   // $
+        collapsed: false
+    },
+    viewer3d: {
+        color: 0xffffff
+    },
+    mesh: {
+        color    : 0x333333,
+        collapsed: false
+    },
+    screen: {
+        width    : 1680,
+        height   : 1050,
+        diagonal : { size: 22, unit: 'in' },
+        collapsed: false
+    },
+    file: {
+        collapsed: false
+    }
 });
-
-// -----------------------------------------------------------------------------
-// UI
-// -----------------------------------------------------------------------------
-var $main     = $('#main');
-var $viewer3d = $('#viewer3d');
 
 // -----------------------------------------------------------------------------
 // Error handler
@@ -19,25 +40,181 @@ function errorHandler(error) {
 }
 
 // -----------------------------------------------------------------------------
-// 3D viewer
-// -----------------------------------------------------------------------------
-// 3D viewer instance
-var viewer3d = new SLAcer.Viewer3D({ target: $viewer3d[0] });
-
-// Full screen
-$(window).resize(function(e) {
-    viewer3d.setSize({
-        width : $main.width(),
-        height: $main.height()
-    });
-    viewer3d.render();
-});
-$(window).resize();
-
-// -----------------------------------------------------------------------------
 // Slicer
 // -----------------------------------------------------------------------------
 var slicer = new SLAcer.Slicer();
+
+// -----------------------------------------------------------------------------
+// UI
+// -----------------------------------------------------------------------------
+// Main container
+var $main = $('#main');
+
+// Viewer 3D
+var $viewer3d = $('#viewer3d');
+var viewer3d  = new SLAcer.Viewer3D({
+    color      : settings.get('viewer3d.color'),
+    buildVolume: settings.get('buildVolume'),
+    target     : $viewer3d[0]
+});
+
+// Slider
+var $sliderInput = $('#slider input');
+
+$sliderInput.slider({ reversed : true }).on('change', function(e) {
+    console.log('slice:', e.value.newValue);
+});
+
+var $sliderElement = $('#slider .slider');
+
+// Init panel
+function initPanel(name) {
+    var id    = _.kebabCase(name);
+    var name  = _.camelCase(name);
+    var $body = $('#' + id + '-body');
+
+    $body.on('hidden.bs.collapse', function () {
+        settings.set(name + '.collapsed', true);
+    });
+
+    $body.on('shown.bs.collapse', function () {
+        settings.set(name + '.collapsed', false);
+    });
+
+    if (settings.get(name + '.collapsed')) {
+        $body.collapse('hide');
+    }
+
+    return $body;
+}
+
+// Unit converter
+function parseUnit(value, unit) {
+    return parseFloat(unit == 'in' ? (value / 25.4) : (value * 25.4));
+}
+
+// File panel
+var $fileBody = initPanel('file');
+
+// Mesh panel
+var $meshBody   = initPanel('mesh');
+var $meshFaces  = $meshBody.find('#mesh-faces');
+var $meshVolume = $meshBody.find('#mesh-volume');
+var $meshWeight = $meshBody.find('#mesh-weight');
+
+function updateMeshInfoUI(mesh) {
+    $meshFaces.html(mesh.geometry.faces.length);
+    $meshVolume.html(parseInt(mesh.getVolume() / 1000)); // cm3/ml
+    $meshWeight.html(0);
+}
+
+// Build volume panel
+var $buildVolumeBody = initPanel('buildVolume');
+var $buildVolumeX    = $buildVolumeBody.find('#build-volume-x');
+var $buildVolumeY    = $buildVolumeBody.find('#build-volume-y');
+var $buildVolumeZ    = $buildVolumeBody.find('#build-volume-z');
+
+function updateBuildVolumeUI() {
+    var buildVolume = settings.get('buildVolume');
+    $buildVolumeX.val(buildVolume.size.x);
+    $buildVolumeY.val(buildVolume.size.y);
+    $buildVolumeZ.val(buildVolume.size.z);
+}
+
+function updateBuildVolumeSettings() {
+    var unit = $('#build-volume input[type=radio]:checked').val();
+
+    if (unit != settings.get('buildVolume.diagonal.unit')) {
+        var size = settings.get('buildVolume.size');
+        $buildVolumeX.val(parseUnit(size.x, unit));
+        $buildVolumeY.val(parseUnit(size.y, unit));
+        $buildVolumeZ.val(parseUnit(size.z, unit));
+    }
+
+    settings.set('buildVolume', {
+        size: {
+            x: $buildVolumeX.val(),
+            y: $buildVolumeY.val(),
+            z: $buildVolumeZ.val()
+        },
+        unit: unit
+    });
+}
+
+$('#build-volume-unit-' + settings.get('buildVolume.unit')).prop('checked', true);
+$('#build-volume input[type=radio]').on('change', updateBuildVolumeSettings);
+$('#build-volume input').on('input', updateBuildVolumeSettings);
+updateBuildVolumeUI();
+
+// Resin panel
+var $resinBody    = initPanel('resin');
+var $resinPrice   = $resinBody.find('#resin-price');
+var $resinDensity = $resinBody.find('#resin-density');
+
+function updateResinUI() {
+    var resin = settings.get('resin');
+
+    $resinDensity.val(resin.density);
+    $resinPrice.val(resin.price);
+}
+
+function updateResinSettings() {
+    settings.set('resin.price'  , $resinPrice.val());
+    settings.set('resin.density', $resinDensity.val());
+}
+
+$('#resin input').on('input', updateResinSettings);
+updateResinUI();
+
+// Screen
+var $screenBody         = initPanel('screen');
+var $screenWidth        = $screenBody.find('#screen-width');
+var $screenHeight       = $screenBody.find('#screen-height');
+var $screenDiagonalSize = $screenBody.find('#screen-diagonal-size');
+
+function updateScreenUI() {
+    var screen = settings.get('screen');
+
+    $screenWidth.val(screen.width);
+    $screenHeight.val(screen.height);
+    $screenDiagonalSize.val(screen.diagonal.size);
+}
+
+function updateScreenSettings() {
+    var unit = $('#screen input[type=radio]:checked').val();
+
+    if (unit != settings.get('screen.diagonal.unit')) {
+        $screenDiagonalSize.val(
+            parseUnit(settings.get('screen.diagonal.size'), unit)
+        );
+    }
+
+    settings.set('screen', {
+        width   : $screenWidth.val(),
+        height  : $screenHeight.val(),
+        diagonal: {
+            size: $screenDiagonalSize.val(),
+            unit: unit
+        }
+    });
+}
+
+$('#screen-diagonal-unit-' + settings.get('screen.diagonal.unit')).prop('checked', true);
+$('#screen input[type=radio]').on('change', updateScreenSettings);
+$('#screen input').on('input', updateScreenSettings);
+updateScreenUI();
+
+// UI resize
+function resizeUI() {
+    var width  = $main.width();
+    var height = $main.height();
+    $sliderElement.height(height - 80);
+    viewer3d.setSize({ width : width, height: height });
+    viewer3d.render();
+}
+
+$(window).resize(resizeUI);
+resizeUI();
 
 // -----------------------------------------------------------------------------
 // STL loader
@@ -52,14 +229,16 @@ loader.onGeometry = function(geometry) {
         slicer.mesh && viewer3d.removeObject(slicer.mesh);
 
         // load new mesh in slicer
-        slicer.loadMesh(new SLAcer.Mesh(geometry, meshMaterial));
+        slicer.loadMesh(new SLAcer.Mesh(geometry, new THREE.MeshPhongMaterial({
+            color: settings.get('mesh.color')
+        })));
 
         // add new mesh and render view
         viewer3d.addObject(slicer.mesh);
         viewer3d.render();
 
         // update mesh info
-        //updateMeshInfo(mesh1);
+        updateMeshInfoUI(slicer.mesh);
 
         // get first slice
         //slice(0);
@@ -75,10 +254,16 @@ loader.onError = errorHandler;
 // -----------------------------------------------------------------------------
 // load example
 // -----------------------------------------------------------------------------
-var xmlhttp = new XMLHttpRequest();
+// example STL file
 var stl = '/stl/Octocat-v2.stl';
 //var stl = '/stl/StressTest.stl';
+
+// Create http request object
+var xmlhttp = new XMLHttpRequest();
+
+// Get the file contents
 xmlhttp.open("GET", window.location + stl);
+
 xmlhttp.onreadystatechange = function() {
     if (xmlhttp.readyState == XMLHttpRequest.DONE) {
         if(xmlhttp.status == 200){
@@ -88,4 +273,5 @@ xmlhttp.onreadystatechange = function() {
         }
     }
 }
+
 xmlhttp.send();
